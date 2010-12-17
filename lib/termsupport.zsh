bc0426cec76be152d0bb36546463fccb4db4554b
Adam Keys <adam@therealadam.com>
2010-12-17 00:12:48 -0600

    More better.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -8,14 +8,14 @@ case "$TERM" in
     }
     ;;
   screen*)
-    preexec () {
-      local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
-      echo -ne "\ek$CMD\e\\"
-      print -Pn "\e]0;%n@%m: $1\a"  # xterm
-    }
-    precmd () {
-      echo -ne "\ekzsh\e\\"
-      print -Pn "\e]0;%n@%m: %~\a"  # xterm
-    }
+    # preexec () {
+    #   local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
+    #   echo -ne "\ek$CMD\e\\"
+    #   print -Pn "\e]0;%n@%m: $1\a"  # xterm
+    # }
+    # precmd () {
+    #   echo -ne "\ekzsh\e\\"
+    #   print -Pn "\e]0;%n@%m: %~\a"  # xterm
+    # }
     ;;
 esac
